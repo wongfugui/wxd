2a62e834560ee45687c80b5feea95774cc3d0b85
can.wang <617620052@qq.com>
2018-07-03 23:53:19 +0800

    init

---
+++ common/header.js
@@ -0,0 +1,14 @@
+Component({
+  data:{
+    userInfo: {},
+    hasUserInfo: false,
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
