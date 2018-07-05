002941ce07d550c3895f76df93858efdbd253852
王粲 <617620052@qq.com>
2018-07-05 17:47:28 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,22 +8,22 @@ Page({
       {
         url: '../../images/s1.png',
         title: '了解一些额外知识，让开发锦上添花',
-        Summary: '',
+        summary: '',
       },
       {
         url: '../../images/s2.png',
         title: 'RESETful API 设计规范',
-        Summary: '',
+        summary: '',
       },
       {
         url: '../../images/s3.png',
         title: '160行代码仿Vue实现极简双向绑定',
-        Summary: '附详细注释',
+        summary: '附详细注释',
       },
       {
         url: '../../images/s4.png',
         title: 'React性能优化',
-        Summary: '虚拟Dom原理浅析',
+        summary: '虚拟Dom原理浅析',
       },
     ]
   },
