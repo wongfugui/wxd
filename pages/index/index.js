24cb0262cd279ef0d54d59af0963b13df65196c7
can.wang <617620052@qq.com>
2018-07-09 00:10:20 +0800

    pulldown refresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,10 +2,112 @@
 //获取应用实例
 const app = getApp()
 
+const dataList1 = [{
+    author: 'SegmentFault',
+    title: '聚焦区块链应用，SegmentFault 黑客马拉松引爆珠三角1',
+    summary: '6月23日，SegmentFault 思否在广州未来社举办黑客马拉松，聚焦区块链应用 DApp 开发。活动吸引了来自珠三角的极客参与，现场也来自硅谷和',
+    date: '6月26日',
+    zan: '10',
+    img: '../../images/1.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: '极客广州——EOS Asia郭达峰担任SegmentFault思否黑客马拉松技术顾问2',
+    summary: '近日备受关注的 EOS 投票率超过 15%，主网激活，已正式上线。EOS Asia 联合创始人郭达峰将担任 SegmentFault 思否区块链黑客马拉松广州站技',
+    date: '6月26日',
+    zan: '323',
+    img: '../../images/2.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: '区块链游戏 Dapp 获得思否黑客马拉松北京站冠军3',
+    summary: '上周末，来自硅谷团队的游戏作品“Crypto Bird - An Implement of NabBoard”获得 SegmentFault 思否区块链黑客马拉松北京站冠军。本次黑客',
+    date: '6月26日',
+    zan: '22',
+    img: '../../images/1.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: 'SegmentFault 在杭成功举办黑客马拉松，发布区块链创新基金4',
+    summary: '上周，SegmentFault 在杭州欧美金融城 G5 创投中心启动全球黑客马拉松，本次活动吸引了众多长三角开发者的关注，有多位嘉宾出席活动进行项目交流',
+    date: '6月26日',
+    zan: '123',
+    img: '../../images/1.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: '聚焦区块链应用，SegmentFault 黑客马拉松引爆珠三角5',
+    summary: '6月23日，SegmentFault 思否在广州未来社举办黑客马拉松，聚焦区块链应用 DApp 开发。活动吸引了来自珠三角的极客参与，现场也来自硅谷和',
+    date: '6月26日',
+    zan: '23',
+    img: '../../images/2.jpg',
+  },
+]
+
+const dataList2 = [{
+    author: 'SegmentFault',
+    title: '极客广州——EOS Asia郭达峰担任SegmentFault思否黑客马拉松技术顾问6',
+    summary: '近日备受关注的 EOS 投票率超过 15%，主网激活，已正式上线。EOS Asia 联合创始人郭达峰将担任 SegmentFault 思否区块链黑客马拉松广州站技',
+    date: '6月26日',
+    zan: '43',
+    img: '../../images/2.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: '区块链游戏 Dapp 获得思否黑客马拉松北京站冠军7',
+    summary: '上周末，来自硅谷团队的游戏作品“Crypto Bird - An Implement of NabBoard”获得 SegmentFault 思否区块链黑客马拉松北京站冠军。本次黑客',
+    date: '6月26日',
+    zan: '54',
+    img: '../../images/1.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: 'SegmentFault 在杭成功举办黑客马拉松，发布区块链创新基金8',
+    summary: '上周，SegmentFault 在杭州欧美金融城 G5 创投中心启动全球黑客马拉松，本次活动吸引了众多长三角开发者的关注，有多位嘉宾出席活动进行项目交流',
+    date: '6月26日',
+    zan: '6',
+    img: '../../images/2.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: '聚焦区块链应用，SegmentFault 黑客马拉松引爆珠三角9',
+    summary: '6月23日，SegmentFault 思否在广州未来社举办黑客马拉松，聚焦区块链应用 DApp 开发。活动吸引了来自珠三角的极客参与，现场也来自硅谷和',
+    date: '6月26日',
+    zan: '55',
+    img: '../../images/1.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: '极客广州——EOS Asia郭达峰担任SegmentFault思否黑客马拉松技术顾问10',
+    summary: '近日备受关注的 EOS 投票率超过 15%，主网激活，已正式上线。EOS Asia 联合创始人郭达峰将担任 SegmentFault 思否区块链黑客马拉松广州站技',
+    date: '6月26日',
+    zan: '66',
+    img: '../../images/2.jpg',
+  },
+]
+
+const dataList3 = [{
+    author: 'SegmentFault',
+    title: '区块链游戏 Dapp 获得思否黑客马拉松北京站冠军11',
+    summary: '上周末，来自硅谷团队的游戏作品“Crypto Bird - An Implement of NabBoard”获得 SegmentFault 思否区块链黑客马拉松北京站冠军。本次黑客',
+    date: '6月26日',
+    zan: '551',
+    img: '../../images/1.jpg',
+  },
+  {
+    author: 'SegmentFault',
+    title: 'SegmentFault 在杭成功举办黑客马拉松，发布区块链创新基金12',
+    summary: '上周，SegmentFault 在杭州欧美金融城 G5 创投中心启动全球黑客马拉松，本次活动吸引了众多长三角开发者的关注，有多位嘉宾出席活动进行项目交流',
+    date: '6月26日',
+    zan: '222',
+    img: '../../images/1.jpg',
+  },
+]
+
 Page({
   data: {
-    slider: [
-      {
+    count: 0,
+    slider: [{
         url: '../../images/s1.png',
         title: '了解一些额外知识，让开发锦上添花',
       },
@@ -22,105 +124,33 @@ Page({
         title: 'React性能优化',
       },
     ],
-    articles: [
-      {
-        author: 'SegmentFault',
-        title: '聚焦区块链应用，SegmentFault 黑客马拉松引爆珠三角',
-        summary: '6月23日，SegmentFault 思否在广州未来社举办黑客马拉松，聚焦区块链应用 DApp 开发。活动吸引了来自珠三角的极客参与，现场也来自硅谷和',
-        date: '6月26日',
-        zan: '10',
-        img: '../../images/1.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '极客广州——EOS Asia郭达峰担任SegmentFault思否黑客马拉松技术顾问',
-        summary: '近日备受关注的 EOS 投票率超过 15%，主网激活，已正式上线。EOS Asia 联合创始人郭达峰将担任 SegmentFault 思否区块链黑客马拉松广州站技',
-        date: '6月26日',
-        zan: '323',
-        img: '../../images/2.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '区块链游戏 Dapp 获得思否黑客马拉松北京站冠军',
-        summary: '上周末，来自硅谷团队的游戏作品“Crypto Bird - An Implement of NabBoard”获得 SegmentFault 思否区块链黑客马拉松北京站冠军。本次黑客',
-        date: '6月26日',
-        zan: '22',
-        img: '../../images/1.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: 'SegmentFault 在杭成功举办黑客马拉松，发布区块链创新基金',
-        summary: '上周，SegmentFault 在杭州欧美金融城 G5 创投中心启动全球黑客马拉松，本次活动吸引了众多长三角开发者的关注，有多位嘉宾出席活动进行项目交流',
-        date: '6月26日',
-        zan: '123',
-        img: '../../images/1.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '聚焦区块链应用，SegmentFault 黑客马拉松引爆珠三角',
-        summary: '6月23日，SegmentFault 思否在广州未来社举办黑客马拉松，聚焦区块链应用 DApp 开发。活动吸引了来自珠三角的极客参与，现场也来自硅谷和',
-        date: '6月26日',
-        zan: '23',
-        img: '../../images/2.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '极客广州——EOS Asia郭达峰担任SegmentFault思否黑客马拉松技术顾问',
-        summary: '近日备受关注的 EOS 投票率超过 15%，主网激活，已正式上线。EOS Asia 联合创始人郭达峰将担任 SegmentFault 思否区块链黑客马拉松广州站技',
-        date: '6月26日',
-        zan: '43',
-        img: '../../images/2.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '区块链游戏 Dapp 获得思否黑客马拉松北京站冠军',
-        summary: '上周末，来自硅谷团队的游戏作品“Crypto Bird - An Implement of NabBoard”获得 SegmentFault 思否区块链黑客马拉松北京站冠军。本次黑客',
-        date: '6月26日',
-        zan: '54',
-        img: '../../images/1.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: 'SegmentFault 在杭成功举办黑客马拉松，发布区块链创新基金',
-        summary: '上周，SegmentFault 在杭州欧美金融城 G5 创投中心启动全球黑客马拉松，本次活动吸引了众多长三角开发者的关注，有多位嘉宾出席活动进行项目交流',
-        date: '6月26日',
-        zan: '6',
-        img: '../../images/2.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '聚焦区块链应用，SegmentFault 黑客马拉松引爆珠三角',
-        summary: '6月23日，SegmentFault 思否在广州未来社举办黑客马拉松，聚焦区块链应用 DApp 开发。活动吸引了来自珠三角的极客参与，现场也来自硅谷和',
-        date: '6月26日',
-        zan: '55',
-        img: '../../images/1.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '极客广州——EOS Asia郭达峰担任SegmentFault思否黑客马拉松技术顾问',
-        summary: '近日备受关注的 EOS 投票率超过 15%，主网激活，已正式上线。EOS Asia 联合创始人郭达峰将担任 SegmentFault 思否区块链黑客马拉松广州站技',
-        date: '6月26日',
-        zan: '66',
-        img: '../../images/2.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: '区块链游戏 Dapp 获得思否黑客马拉松北京站冠军',
-        summary: '上周末，来自硅谷团队的游戏作品“Crypto Bird - An Implement of NabBoard”获得 SegmentFault 思否区块链黑客马拉松北京站冠军。本次黑客',
-        date: '6月26日',
-        zan: '551',
-        img: '../../images/1.jpg',
-      },
-      {
-        author: 'SegmentFault',
-        title: 'SegmentFault 在杭成功举办黑客马拉松，发布区块链创新基金',
-        summary: '上周，SegmentFault 在杭州欧美金融城 G5 创投中心启动全球黑客马拉松，本次活动吸引了众多长三角开发者的关注，有多位嘉宾出席活动进行项目交流',
-        date: '6月26日',
-        zan: '222',
-        img: '../../images/1.jpg',
-      },
-    ]
+    articles: [...dataList1]
   },
-  onLoad: function () {
+  onLoad: function() {
+
   },
+
+  onPullDownRefresh: function() {
+    setTimeout(()=>{
+      if (this.data.articles.length < 10){
+        this.setData({articles: this.data.articles.concat(dataList2)})
+        wx.stopPullDownRefresh()
+        return
+      }
+      if (dataList3.length < 12) {
+        this.setData({ articles: this.data.articles.concat(dataList3) })
+        wx.stopPullDownRefresh()
+        return
+      }
+      console.log('no more')
+      wx.stopPullDownRefresh()
+    }, 1000)
+  },
+
+  onReachBottom: function() {
+    wx.showToast({
+      title: '已经到底啦！',
+      icon: 'none'
+    })
+  }
 })
