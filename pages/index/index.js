bfd9b759b2e27b4e2eee674c692e4ff6b4f7da8f
can.wang <617620052@qq.com>
2018-07-10 23:01:33 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,9 +2,12 @@
 //获取应用实例
 const app = getApp()
 
+const globalData = getApp().globalData
+const bmap = require('../../lib/bmap-wx.js')
+
 Page({
   data: {
-
+    bcgColor: '#40a7e7',
   },
   onLoad: function() {
 
