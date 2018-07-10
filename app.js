384834267185f4d4418c57602eb2e4a7d505b048
王粲 <617620052@qq.com>
2018-07-10 17:56:02 +0800

    update

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 //app.js
 App({
   onLaunch: function () {
-
+    wx.getSystemInfo({
+      success:  (res) => {
+        this.globalData.systeminfo = res
+      },
+    })
   },
   globalData: {
+    // 是否保持常亮，离开小程序失效
+    keepscreenon: false,
+    systeminfo: {},
     ak: 'jc1dIcPqBa8nmKfDwx9u8lGS13cF35d4',
   }
 })
