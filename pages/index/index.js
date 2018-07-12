b3faff8ad8b321bad56b0a0992ea803de2a420df
王粲 <617620052@qq.com>
2018-07-12 18:01:16 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,30 +8,40 @@ const bmap = require('../../lib/bmap-wx.js')
 Page({
   data: {
     bcgColor: '#40a7e7',
+    weatherData: {},
+    temp: '',
   },
   onLoad: function() {
-
+    this.getWeather()
   },
 
-  onShow: function (){
-    this.getWeather()
+  onShow: function() {
+
   },
 
   onPullDownRefresh(res) {
     this.getWeather()
   },
 
-  getWeather: function(location){
+  getWeather: function(location) {
     const BMap = new bmap.BMapWX({
       ak: globalData.ak
     })
     const success = (data) => {
-      console.log(data)
+      const weatherData = data.originalData.results[0]
+      const temp = weatherData.weather_data[0].date.match(/\d+/g)[2]
+      const weather = weatherData.weather_data[0].weather
+      this.setData({
+        weatherData,
+        temp,
+        weather
+      })
+      console.log(weather)
     }
     BMap.weather({
       location,
       success
-    }); 
+    });
   }
 
 
